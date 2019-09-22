aa43de0601a787409f1c2a7f14b9a7432505e657
The Void <MawOfTheVoid@protonmail.com>
2019-09-22 22:36:38 +0200

    There is now a screen!

--- source/chip8_emulator.py
+++ source/chip8_emulator.py
@@ -1,10 +1,12 @@
 import binascii
+import pygame
+import sys
 
 class Chip8():
 
     def __init__(self, rom_path, screen):
         self.rom_path = rom_path
-        self.screen_matrix = screen.screen_matrix
+        self.screen = screen
         self.memory = self.prepare_memory()
         self.pc = 512
         self.I_register = 0
@@ -15,7 +17,24 @@ class Chip8():
         self.opcode = ""
 
     def get_input(self):
+        for event in pygame.event.get():
+            self.system_relevant_keypress(event)
+            self.game_keydown(event)
+            self.game_keyup(event)
+
+    def system_relevant_keypress(self, event):
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                self.screen.toggle_fullscreen()
+
+    def game_keydown(self, event):
         pass
+
+    def game_keyup(self, event):
+        pass
+
 
     def fetch_opcode(self):
         pass
--- source/screen.py
+++ source/screen.py
@@ -1,9 +1,38 @@
+import pygame
+
+
 class Screen():
 
     def __init__(self, on_pixel_color, off_pixel_color):
-        self.screen_matrix = []
+        self.screen_matrix = [[0 for x in range(64)] for y in range(32)]
         self.on_pixel_color = on_pixel_color
         self.off_pixel_color = off_pixel_color
+        self.rect_width = 0
+        self.rect_height = 0
+        self.screen_width = 800
+        self.screen_height = 400
+        self.is_fullscreen = False
+        pygame.display.set_icon(pygame.image.load("Chip8Boy.png"))
+        pygame.display.set_caption("Chip 8")
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height),
+            flags=pygame.RESIZABLE)
+
+    def toggle_fullscreen(self):
+        if self.is_fullscreen:
+            pygame.display.set_mode(
+                (self.screen_width, self.screen_height),
+                flags=pygame.RESIZABLE)
+            self.update_rect_sizes()
+            self.is_fullscreen = False
+        elif self.is_fullscreen is False:
+            pygame.display.set_mode(
+                (0, 0), flags=pygame.FULLSCREEN)
+            self.update_rect_sizes()
+            self.is_fullscreen = True
+
+    def update_rect_sizes(self):
+        pass
 
     def draw_matrix(self):
         pass
