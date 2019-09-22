46db35e8f44f4a62aa92dd9a96506424e029e2f2
The Void <MawOfTheVoid@protonmail.com>
2019-09-22 23:41:37 +0200

    Changed memory from string based to int based while still possible to increase performance.

--- source/chip8_emulator.py
+++ source/chip8_emulator.py
@@ -14,7 +14,7 @@ class Chip8():
         self.delay_timer = 0
         self.music_timer = 0
         self.keyboard_register = []
-        self.opcode = ""
+        self.opcode = 0
 
     def get_input(self):
         for event in pygame.event.get():
@@ -40,7 +40,11 @@ class Chip8():
         pass
 
     def fetch_opcode(self):
-        pass
+        part_one = self.memory[self.pc]
+        part_two = self.memory[self.pc+1]
+        part_one = part_one << 8
+        self.opcode = part_one + part_two
+        self.pc += 2
 
     def execute_opcode(self):
         pass
@@ -53,7 +57,7 @@ class Chip8():
         memory += 432 * [0]
         memory += self.load_rom(memory)
         memory += (4096 - len(memory)) * [0]
-        self.memory = memory
+        return memory
 
     def load_rom(self, memory):
         with open(self.rom_path, "rb") as file_obj:
@@ -62,29 +66,27 @@ class Chip8():
         # split rom into two hex values each
         rom = [rom[i:i + 2] for i in range(0, len(rom), 2)]
         # converts them from binary string to normal string
-        rom = [hex(int(register, 16)) for register in rom]
+        rom = [int(register, 16) for register in rom]
         return rom
 
 
     def memory_fonts(self):
         fonts = [
-            # font for 0
-            "0xF0", "0x90", "0x90", "0x90", "0xF0",
-            # font for 1
-            "0x20", "0x60", "0x20", "0x20", "0x70",
-            "0xF0", "0x10", "0xF0", "0x80", "0xF0",
-            "0xF0", "0x10", "0xF0", "0x10", "0xF0",
-            "0x90", "0x90", "0xF0", "0x10", "0x10",
-            "0xF0", "0x80", "0xF0", "0x10", "0xF0",
-            "0xF0", "0x80", "0xF0", "0x90", "0xF0",
-            "0xF0", "0x10", "0x20", "0x40", "0x40",
-            "0xF0", "0x90", "0xF0", "0x90", "0xF0",
-            "0xF0", "0x90", "0xF0", "0x10", "0xF0",
-            "0xF0", "0x90", "0xF0", "0x90", "0x90",
-            "0xE0", "0x90", "0xE0", "0x90", "0xE0",
-            "0xF0", "0x80", "0x80", "0x80", "0xF0",
-            "0xE0", "0x90", "0x90", "0x90", "0xE0",
-            "0xF0", "0x80", "0xF0", "0x80", "0xF0",
-            "0xF0", "0x80", "0xF0", "0x80", "0x80",
+            240, 144, 144, 144, 240,
+            32, 96, 32, 32, 112,
+            240, 16, 240, 128, 240,
+            240, 16, 240, 16, 240,
+            144, 144, 240, 16, 16,
+            240, 128, 240, 16, 240,
+            240, 128, 240, 144, 240,
+            240, 16, 32, 64, 64,
+            240, 144, 240, 144, 240,
+            240, 144, 240, 16, 240,
+            240, 144, 240, 144, 144,
+            224, 144, 224, 144, 224,
+            240, 128, 128, 128, 240,
+            224, 144, 144, 144, 224,
+            240, 128, 240, 128, 240,
+            240, 128, 240, 128, 128
         ]
         return fonts
